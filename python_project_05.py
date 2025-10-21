b9de23555539b5cd051b703a6cf49b6278d7a65d
Masum <masumfaa69@gmail.com>
2025-10-21 10:31:36 +0600

    Random New Project

---
+++ python_project_05.py
@@ -0,0 +1,48 @@
+#create a new random file
+
+# import random
+
+# subjects = [
+#     "shakib khan",
+#     "amir kahn",
+#     "Jamat a amir Dc. Shofiqur rohman",
+#     "Prime minister Modi",
+#     "I'm very hungry",
+#     "Mere Desh bohot jamela",
+#     "Aotu Riskar diver of dhaka"
+# ]
+
+# actions = [
+#     "launchues",
+#     "celebrates",
+#     "dance with",
+#     "eats",
+#     "decliars",
+#     "orders",
+#     "world war"
+# ]
+
+# place_ananounting = [
+#     "at red fort",
+#     "in Mumbai local trin",
+#     "in ploate of somosa",
+#     "inside parlamant",
+#     "a gange chat",
+#     "the part of trank",
+#     "thing the advance nirbacon"
+# ]
+# while True:
+
+#     subject = random.choice(subjects)
+#     action = random.choice(actions)
+#     place = random.choice(place_ananounting)
+
+#     heading = f'BRACKING NEWS: {subject} {action} {place} '
+#     print('\n ' + heading)
+
+#     user_input = input('\n do you want annther headling (Yes/No)?: ').strip()
+#     if user_input == 'no':
+#         break
+
+# print("\n Create the random number")
+
