99d1f8ba3af90165a890d3dc194b2b49bcf99910
Masum <masumfaa69@gmail.com>
2025-10-21 11:26:50 +0600

    The 2 Mini Project added

--- python_project_05.py
+++ python_project_05.py
@@ -46,3 +46,46 @@
 
 # print("\n Create the random number")
 
+
+
+##computer python restaurant
+
+
+menu = {
+    "Pizza": 40,
+    "Pastha": 50,
+    "Burger": 70,
+    "Salat": 30,
+    "Coffee": 40
+}
+
+print(f'-----Welcome To Our Restaurant------')
+print(f'Our Item Active:\n Pizza: TK40\n Pastha: 50TK\n Burger: 70TK\n Salat: 30TK\n Coffee: 40TK')
+
+taotal_amount = 0
+
+user_need_item01 = input("Enter the name of item you want order: ").capitalize()
+if user_need_item01 in menu:
+    taotal_amount += menu[user_need_item01]
+    print("thanks the order") 
+else:
+    print(f'The item not available! Choice another item')
+
+
+another_item = input("Do you need another item (Yes/No)?: ").capitalize()
+if another_item == 'Yes':
+    user_need_item02 = input("Enter your new item, do you want: ").capitalize()
+    if user_need_item02 in menu:
+        taotal_amount += menu[user_need_item02]
+        print("thank for again the order") 
+    else:
+        print("Not available! please try agin")
+
+ 
+
+print(f'Your Total Rastaurant Bill: {taotal_amount}')
+
+
+
+
+
